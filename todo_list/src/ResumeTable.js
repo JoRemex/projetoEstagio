2dc0c1e67d69b3d5988c66483509bf0bc8f871e0
JoRemex <joelremesso96@gmail.com>
2022-06-30 17:21:27 +0100

    grid
    Foram feitas das tarefas em grid e com cores associadas à prioridade, aparece em cada toDo o titulo e tarefa, 3 no máximo por linha. Falta colocar máximo de tarefas no ecra resumo e melhorar aspeto

--- todo_list/src/ResumeTable.js
+++ todo_list/src/ResumeTable.js
@@ -1,38 +1,33 @@
 //----abaixo estão os imports da tabela
-import Paper from "@mui/material/Paper";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
+import { Stack } from "@mui/material";
+import Grid from "@mui/material/Grid";
 import { useSelector } from "react-redux";
 export function TabelaSimples() {
   const data = useSelector((store) => store.array);
   return (
-    <TableContainer component={Paper} className="tabela">
-      <Table sx={{ minWidth: 500 }} size="small" aria-label="a dense table">
-        <TableHead>
-          <TableRow>
-            <TableCell align="left">Título</TableCell>
-            <TableCell align="center">Prioridade</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {data.map((row) => (
-            
-            <TableRow
-              key={row.length}
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }} //apagar esta linha
-            >
-              <TableCell component="th" scope="row">
-                {row.title}
-              </TableCell>
-              <TableCell align="center">{row.priority}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <>
+      <Grid className="grelha">
+        {!data.length ? (
+          <div className="vazio">De momento não tem nenhuma toDo</div>
+        ) : (
+          data.map((row) => (
+            <Stack className={row.priority}>
+              <Stack
+                spacing={2}
+                className="cartao"
+                alignItems="center"
+                justifyContent="space-between"
+                padding="1rem 5rem"
+              >
+                <Stack paddingTop="0px">{row.title}</Stack>
+                <Stack alignItems="center" justifyContent="center">
+                  {row.describe}
+                </Stack>
+              </Stack>
+            </Stack>
+          ))
+        )}
+      </Grid>
+    </>
   );
 }
